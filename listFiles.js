f77582ce215e4cea1c3a7960eb4e53f40aefa004
Adrian Stabiszewski <as@grundid.de>
2018-03-17 17:18:29 +0100

    added script

--- listFiles.js
+++ listFiles.js
@@ -1,9 +1,12 @@
 const fs = require("fs");
+if (process.argv.length === 4) {
+    const files = fs.readdirSync(process.argv[2]);
+    const last48h = files.slice(files.length - 12 * 48, files.length);
+    fs.writeFileSync(process.argv[3]+"files.json", JSON.stringify(last48h));
+} else {
+    console.log("USAGE: node listFiles.js <dir> <output>")
+}
 
 
-const files = fs.readdirSync("lastdays/");
 
-const last48h = files.slice(files.length - 12 * 48, files.length);
-
-console.log(files);
 
